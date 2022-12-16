15d2ce0cbcec6e3a4e4b15aa1884c214fb26a150
AlyonaUkr <mochulska1994@gmail.com>
2022-12-16 15:11:07 +0200

    arithmetic mean

---
+++ scripts.js
@@ -0,0 +1,7 @@
+const firstNumber = Number(prompt('Write first number, please'));
+const secondNumber = Number(prompt('Write second number, please'));
+const thirdNumber = Number(prompt('Write third number, please'));
+const userResults = (firstNumber + secondNumber + thirdNumber) / 3;
+
+
+alert(userResults);
